ca52849675fffd9131f581a866d26461b4d66fb2
Oleksii Liubarets <lubaretsaa@gmail.com>
2017-08-11 02:16:35 +0300

    no message

---
+++ Pods/Target Support Files/Pods-GalleryFB/Pods-GalleryFB-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GalleryFB : NSObject
+@end
+@implementation PodsDummy_Pods_GalleryFB
+@end
---
+++ Pods/Target Support Files/Pods-GalleryFBUITests/Pods-GalleryFBUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GalleryFBUITests : NSObject
+@end
+@implementation PodsDummy_Pods_GalleryFBUITests
+@end
