ef6f12fc785b3faeaf13348995fdf5d6508d823d
Oleksii Liubarets <lubaretsaa@gmail.com>
2017-08-13 15:28:27 +0300

    photos in album added need to fix count of photos

--- GalleryFB/AlbumsTableViewController.swift
+++ GalleryFB/AlbumsTableViewController.swift
@@ -32,9 +32,7 @@ class AlbumsTableViewController: UITableViewController {
         } else {
             let loginVC = self.storyboard?.instantiateViewController(withIdentifier: "LoginViewController") as! LoginViewController
             self.present(loginVC, animated: true, completion: nil)
-
         }
-    
     }
     
     func fetchProfile() {
@@ -54,8 +52,9 @@ class AlbumsTableViewController: UITableViewController {
                     let cover = album["picture"] as! [String : Any]
 //                    print(cover["data"])
                     let coverData = cover["data"] as! [String : Any]
+//                    print(coverData)
                     let urlString = coverData["url"] as! String
-                    print(urlString)
+//                    print(urlString)
                     let url = URL.init(string: urlString)
                     let data = try! Data.init(contentsOf: url!)
                     let picture = UIImage.init(data: data)
@@ -65,7 +64,7 @@ class AlbumsTableViewController: UITableViewController {
 //                print(self.albums)
 //                print(self.albums[2].albumName)
 //                print(self.albums.count)
-                print(self.albums[0].albumCover)
+//                print(self.albums[0].albumCover)
                 
             } else {
                 
@@ -75,7 +74,7 @@ class AlbumsTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+
         return albums.count
     }
 
@@ -87,53 +86,27 @@ class AlbumsTableViewController: UITableViewController {
         cell.albumNameLabel.text = album.albumName
         cell.albumCoverImage.image = album.albumCover
         
-    
         return cell
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let album = albums[indexPath.row]
+        let nameOfAlbum = album.albumName
+        
+        performSegue(withIdentifier: "PhotosInAlbumTableViewController", sender: nameOfAlbum)
     }
-    */
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "PhotosInAlbumTableViewController" {
+            if let controller = segue.destination as? PhotosInAlbumTableViewController,
+                let nameOfAlbum = sender as? String {
+                
+                controller.album = nameOfAlbum
+            }
+        }
     }
-    */
 
 }
--- GalleryFB/PhotosInAlbumTableViewController.swift
+++ GalleryFB/PhotosInAlbumTableViewController.swift
@@ -7,72 +7,91 @@
 //
 
 import UIKit
+import FacebookCore
+import FBSDKCoreKit
+import FBSDKLoginKit
+import FacebookLogin
 
 class PhotosInAlbumTableViewController: UITableViewController {
 
+    var dict: [String: AnyObject]!
+    var photos = [Photos]() {
+        didSet{
+            self.tableView.reloadData()
+        }
+    }
+    
+    var album: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        fetchProfile()
+    }
+    
+    func fetchProfile() {
+        print("fetch profile photos")
+        
+        let parameters = ["fields" : "name,photos{images}"]
+        FBSDKGraphRequest(graphPath: "me/albums", parameters: parameters).start { (connection, result, error) in
+            
+            if let resultJSON = result, error == nil {
+                
+                self.dict = resultJSON as! [String : AnyObject]
+//                print(resultJSON)
+                
+                let albums = self.dict["data"] as! [[String : Any]]
+//                print(albums)
+                
+                for album in albums {
+                    let name = album["name"] as! String
+                    if name == self.album {
+                        print(name)
+                        let photos = album["photos"] as! [String : Any]
+//                        print(photos["data"])
+                        let photosData = photos["data"] as! [[String : Any]]
+//                        print(photosData)
+                        
+                        for photo in photosData {
+//                            print(photo)
+                            let images = photo["images"] as! [[String : Any]]
+                            
+//                            print(images)
+                            for image in images {
+                                print(image)
+                                let urlString = image["source"] as! String
+                                let url = URL.init(string: urlString)
+                                let data = try! Data.init(contentsOf: url!)
+                                let picture = UIImage.init(data: data)
+                                let model = Photos(photo: picture!)
+                                self.photos.append(model)
+                            }
+//                            let image = images["source"] as! [String : Any]
+//                            print(image)
+                        }
+                        
+                    }
+//                    print(name)
+                }
+            
+            }
+        }
     }
 
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        return photos.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotosCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotosCell", for: indexPath) as! PhotosTableViewCell
 
+        let photo = photos[indexPath.row]
         
+        cell.photoImage.image = photo.photo
         
         return cell
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
